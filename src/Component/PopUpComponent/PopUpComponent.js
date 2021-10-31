b86a91e600ef54148f8deaaec8ec3a1df0d66a58
dheeraj1429 <dheerajsingh1429@gmail.com>
2021-10-31 08:29:56 +0530

    navbar reuseble component created

--- src/Component/PopUpComponent/PopUpComponent.js
+++ src/Component/PopUpComponent/PopUpComponent.js
@@ -1,34 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
 
 import PopUpInnerFirstComponent from "../PopUpInnerComponent/PopUpInnerFirstComponent/PopUpInnerFirstComponent";
 
 import "./PopUpComponent.css";
 
-function PopUpcomponent(props) {
+function PopUpcomponent({ ContentData, ClassData, SideBarContentPopUp }) {
   const [CollectionData, setCollectionData] = useState({
-    collection: {},
+    MegaNavCollection: {},
+    UserCollectionContent: {},
   });
 
   useEffect(() => {
-    if (props.ContentData !== null) {
-      const { firstContent, secondContent, threeContent } = props.ContentData;
-      setCollectionData({ collection: { firstContent, secondContent, threeContent } });
+    if (ContentData !== null) {
+      const { firstContent, secondContent, threeContent } = ContentData.navInnerContent;
+      setCollectionData({ MegaNavCollection: { firstContent, secondContent, threeContent }, UserCollectionContent: [ContentData.navBarUserContent] });
     }
   }, []);
 
-  return (
-    <div className={props.ClassData ? `MenuInner ${props.ClassData}` : "MenuInner"}>
-      <div className="ArrowLine"></div>
-      {props.ContentData !== null ? (
+  if (CollectionData.MegaNavCollection && SideBarContentPopUp == "MenuMegaBar") {
+    return (
+      <div className={ClassData ? `MenuInner ${ClassData}` : "MenuInner"}>
+        <div className="ArrowLine"></div>
+
         <div className="PopUpInnerContent">
-          <PopUpInnerFirstComponent InnerData={CollectionData.collection.firstContent} />
-          <PopUpInnerFirstComponent InnerData={CollectionData.collection.secondContent} />
-          <PopUpInnerFirstComponent InnerData={CollectionData.collection.threeContent} />
+          <PopUpInnerFirstComponent InnerData={CollectionData.MegaNavCollection.firstContent} />
+          <PopUpInnerFirstComponent InnerData={CollectionData.MegaNavCollection.secondContent} />
+          <PopUpInnerFirstComponent InnerData={CollectionData.MegaNavCollection.threeContent} />
         </div>
-      ) : null}
-    </div>
-  );
+      </div>
+    );
+  }
+
+  if (CollectionData.UserCollectionContent && SideBarContentPopUp == "UserDetails") {
+    return (
+      <div className={ClassData ? `MenuInner ${ClassData}` : "MenuInner"}>
+        <div className="ArrowLine"></div>
+
+        <div className="PopUpInnerContent">
+          <PopUpInnerFirstComponent InnerData={CollectionData.UserCollectionContent[0]} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PopUpcomponent;
--- src/Component/PopUpInnerComponent/PopUpInnerFirstComponent/PopUpInnerFirstComponent.js
+++ src/Component/PopUpInnerComponent/PopUpInnerFirstComponent/PopUpInnerFirstComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./PopUpInnerFirstComponent.css";
 
@@ -6,7 +7,17 @@ function PopUpInnerFirstComponent({ InnerData }) {
   return (
     <div className="PopUpInnerDiv">
       <div className="PopUpContentDiv">
-        {InnerData !== undefined ? InnerData.map((el) => (el.heading ? <h3>{el.heading}</h3> : <p>{el.title}</p>)) : null}
+        {InnerData !== undefined
+          ? InnerData.map((el) =>
+              el.heading ? (
+                <h3>{el.heading}</h3>
+              ) : (
+                <Link to={el.title.replaceAll(" ", "_")}>
+                  <p>{el.title}</p>
+                </Link>
+              )
+            )
+          : null}
       </div>
     </div>
   );
